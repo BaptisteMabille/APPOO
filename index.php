4f526d2fef88d4c72945a25a6540938224b6438e
Baptiste Mabille <baptiste.mabille@viacesi.fr>
2018-03-15 17:25:41 +0100

    Commit globale

--- index.php
+++ index.php
@@ -2,13 +2,19 @@
 include "./functions/classAutoLoader.php";
 spl_autoload_register("classAutoLoader");
 
+$guepard = new Koala();
+$guepard -> emetreSon();
+$guepard -> seNourrir();
+
+var_dump($guepard);
+
+
+/*
 $voiture1 = new Voiture(1000);
 
-var_dump($voiture1);
 
 $voiture1 -> couleur = "bleue";
 
-var_dump($voiture1);
 
 $voiture1 -> vitesse = 28;
 $voiture1 -> masse = 1000;
@@ -17,5 +23,7 @@ if($ec = $voiture1 -> calculerEnergieCinetique()) {
     $ec = $ec . " Joules";
 
     Log::logWrite($ec);
-}
+}*/
+
+
 
